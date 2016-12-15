b8804dcc84f08ca081298a34c8943e9ab7b2fc9d
okt2015 <onkar.tiwari@gmail.com>
2016-12-15 15:32:13 +0530

    Add project files.

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication10.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World 1" << endl;
+	return 0;
+}
